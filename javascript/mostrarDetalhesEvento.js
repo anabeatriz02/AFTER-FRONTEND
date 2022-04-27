d8253347331e2281d981fd3616696a829e9f0c35
Felipe de Melo Fernandes <79909903+FelipeMel0@users.noreply.github.com>
2022-04-27 08:56:44 -0300

    Tratando dados para listagem

--- javascript/mostrarDetalhesEvento.js
+++ javascript/mostrarDetalhesEvento.js
@@ -1,7 +1,7 @@
-async function getContent(){
+async function getContent() {
     try {
 
-        const response = await fetch('http://localhost:4000/evento/acharEventoIdEvento/2')
+        const response = await fetch('http://localhost:4000/evento/acharEventoIdEvento/5')
 
         console.log(response)
 
@@ -16,31 +16,93 @@ async function getContent(){
         console.error(error)
 
     }
-   
+
 }
 
 getContent()
 
-function mostrarDetalhes(evento){
+function mostrarDetalhes(evento) {
 
     //Mostrar título e celebridade
-    titulo = `${evento.titulo}<span>com</span><a href="">${evento.tblIntermEventoCelebridades[0].tblCelebridade.tblVerificacaoUsuario.nickname}<img src="../img/icon-check.svg" /></a>`
+    if (evento.tblIntermEventoCelebridades[0] == undefined) {
+        titulo = `${evento.titulo}`
+    } else {
+        titulo = `${evento.titulo}<span>com</span><a href="">${evento.tblIntermEventoCelebridades[0].tblCelebridade.tblVerificacaoUsuario.nickname}<img src="../img/icon-check.svg" /></a>`
+    }
 
     document.querySelector("#tituloAutor").innerHTML = titulo
 
     //Mostrar hora de início e hora de término
-    duracao = `Sexta às ${evento.horaInicio} e ${evento.horaFim}`
+    function formatHour(input) {
+        var hourPart = input.match(/\d+/g),
+            minuto = hourPart[1],
+            hora = hourPart[0];
+        return hora + 'h' + minuto;
+    }
+
+    duracao = `De ${formatHour(evento.horaInicio)} a ${formatHour(evento.horaFim)}`
 
     document.querySelector("#horarioEvento").innerHTML = duracao
 
 
     //Mostrar data do evento
-    dataEvento = `${evento.dataInicio} a ${evento.dataFim}`
+
+    function formatDate(input) {
+        var datePart = input.match(/\d+/g),
+            year = datePart[0].substring(0),
+            month = datePart[1],
+            day = datePart[2];
+
+        if (month == 1) {
+            month = "janeiro"
+        } else if (month == 2) {
+            month = "fevereiro"
+        } else if (month == 3) {
+            month = "março"
+        } else if (month == 4) {
+            month = "abril"
+        } else if (month == 5) {
+            month = "maio"
+        } else if (month == 6) {
+            month = "junho"
+        } else if (month == 7) {
+            month = "julho"
+        } else if (month == 8) {
+            month = "agosto"
+        } else if (month == 9) {
+            month = "setembro"
+        } else if (month == 10) {
+            month = "outubro"
+        } else if (month == 11) {
+            month = "novembro"
+        } else if (month == 12) {
+            month = "dezembro"
+        }
+
+        // return day + '/' + month + '/' + year;
+        return day + ' de ' + month + ' de ' + year;
+    }
+
+    if(evento.dataFim == evento.dataInicio) {
+        dataEvento = `${formatDate(evento.dataInicio)}`
+    } else {
+        dataEvento = `${formatDate(evento.dataInicio)} a ${formatDate(evento.dataFim)}`
+    }
+
 
     document.querySelector("#dataEvento").innerHTML = dataEvento
 
     //Mostrar endereço do evento
-    endereco = `${evento.tblEnderecoEventos[0].logradouro} - ${evento.tblEnderecoEventos[0].bairro}, ${evento.tblEnderecoEventos[0].cidade} - ${evento.tblEnderecoEventos[0].estado} - ${evento.tblEnderecoEventos[0].complemento}`
+
+    if (evento.tblEnderecoEventos[0].complemento == null || evento.tblEnderecoEventos[0].complemento == "") {
+
+        endereco = `${evento.tblEnderecoEventos[0].logradouro} - ${evento.tblEnderecoEventos[0].bairro}, ${evento.tblEnderecoEventos[0].cidade} - ${evento.tblEnderecoEventos[0].estado}`
+
+    } else {
+
+        endereco = `${evento.tblEnderecoEventos[0].logradouro} - ${evento.tblEnderecoEventos[0].bairro}, ${evento.tblEnderecoEventos[0].cidade} - ${evento.tblEnderecoEventos[0].estado} - ${evento.tblEnderecoEventos[0].complemento}`
+
+    }
 
     document.querySelector("#enderecoEvento").innerHTML = endereco
 
