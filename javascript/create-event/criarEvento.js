3a4569d01f862eda39f88750cbde303178d178a6
Fernanda Ferreira <79976892+fernandaferreiras@users.noreply.github.com>
2022-05-03 09:57:47 -0300

    first-axios

--- javascript/create-event/criarEvento.js
+++ javascript/create-event/criarEvento.js
@@ -1,139 +1,159 @@
-const btn = document.querySelector('#salvar')
+const btn = document.querySelector("#salvar");
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
+	const evento = getDadosEvento();
 
-    const evento = getDadosEvento()
-
-    enviarEventoParaAPI(evento)
-    // console.log(evento)
-
-})
-
+	enviarEventoParaAPI(evento);
+});
 
 function getDadosEvento() {
-    const inputTitulo = document.querySelector('#titulo')
+	const inputTitulo = document.querySelector("#titulo");
 
-    const selectCategoria = document.querySelector('#category')
-    const categoriaSelecionada = selectCategoria.options[selectCategoria.selectedIndex].value
+	const selectCategoria = document.querySelector("#category");
+	const categoriaSelecionada = selectCategoria.options[selectCategoria.selectedIndex].value;
 
-    // const selectAssunto = document.querySelector('#topic')
-    // const assuntoSelecionado = selectAssunto.options[selectAssunto.selectedIndex].value
+	// const selectAssunto = document.querySelector('#topic')
+	// const assuntoSelecionado = selectAssunto.options[selectAssunto.selectedIndex].value
 
-    const selectTipoEvento = document.querySelector('#tipoEvento')
-    const tipoEventoSelecionado = selectTipoEvento.options[selectTipoEvento.selectedIndex].value
+	const selectTipoEvento = document.querySelector("#tipoEvento");
+	const tipoEventoSelecionado = selectTipoEvento.options[selectTipoEvento.selectedIndex].value;
 
-    const inputDescricao = document.querySelector('#descricao')
+	const inputDescricao = document.querySelector("#descricao");
 
-    const inputCep = document.querySelector('#cep')
-    const inputBairro = document.querySelector('#bairro')
-    const inputEstado = document.querySelector('#estado')
-    const inputLogradouro = document.querySelector('#logradouro')
-    const inputCidade = document.querySelector('#cidade')
+	const inputCep = document.querySelector("#cep");
+	const inputBairro = document.querySelector("#bairro");
+	const inputEstado = document.querySelector("#estado");
+	const inputLogradouro = document.querySelector("#logradouro");
+	const inputCidade = document.querySelector("#cidade");
 
-    const inputDataInicio = document.querySelector('#dataInicio')
-    const inputDataFim = document.querySelector('#dataFim')
-    const inputHoraInicio = document.querySelector('#horaInicio')
-    const inputHoraFim = document.querySelector('#horaFim')
+	const inputDataInicio = document.querySelector("#dataInicio");
+	const inputDataFim = document.querySelector("#dataFim");
+	const inputHoraInicio = document.querySelector("#horaInicio");
+	const inputHoraFim = document.querySelector("#horaFim");
 
-    const inputCapa = document.querySelector('#input-photo-file')
+	const inputCapa = document.querySelector("#input-photo-file");
 
-    if (inputDataFim.value == null || inputDataFim.value == "") {
-        dataTermino = inputDataInicio.value
-    } else {
-        dataTermino = inputDataFim.value
-    }
+	if (inputDataFim.value == null || inputDataFim.value == "") {
+		dataTermino = inputDataInicio.value;
+	} else {
+		dataTermino = inputDataFim.value;
+	}
 
-    const evento = {
-        titulo: inputTitulo.value,
-        descricao: inputDescricao.value,
-        tblCategoriumIdCategoria: categoriaSelecionada,
-        dataInicio: inputDataInicio.value,
-        dataFim: dataTermino,
-        horaInicio: inputHoraInicio.value,
-        horaFim: inputHoraFim.value,
-        capa: inputCapa.value,
-        cep: inputCep.value,
-        logradouro: inputLogradouro.value,
-        bairro: inputBairro.value,
-        cidade: inputCidade.value,
-        estado: inputEstado.value,
-        tblTipoEventoIdTipoEvento: tipoEventoSelecionado
-    }
+	const evento = {
+		titulo: inputTitulo.value,
+		descricao: inputDescricao.value,
+		tblCategoriumIdCategoria: categoriaSelecionada,
+		dataInicio: inputDataInicio.value,
+		dataFim: dataTermino,
+		horaInicio: inputHoraInicio.value,
+		horaFim: inputHoraFim.value,
+		capa: inputCapa.value,
+		cep: inputCep.value,
+		logradouro: inputLogradouro.value,
+		bairro: inputBairro.value,
+		cidade: inputCidade.value,
+		estado: inputEstado.value,
+		tblTipoEventoIdTipoEvento: tipoEventoSelecionado,
+	};
 
-    return evento
+	return evento;
 }
 
-async function enviarEventoParaAPI(evento) {
-    try {
-        const resposta = await fetch('http://localhost:4000/evento/cadastrarEventoEndereco/1/', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(evento)
-        })
+// btn.addEventListener("click", () => {
+// 	const email = emailInput.value;
+// 	const password = passwordInput.value;
 
-    } catch (erro) {
-        console.error(erro)
-    }
+// 	axios
+// 		.post("https://reqres.in/api/login", {
+// 			email: email,
+// 			password: password,
+// 		})
+// 		.then((response) => {
+// 			console.log(response);
+// 		});
+// });
+
+async function enviarEventoParaAPI(evento) {
+	axios
+		.post("http://localhost:4000/evento/cadastrarEventoEndereco/1/", {
+			titulo: inputTitulo,
+			descricao: inputDescricao,
+			tblCategoriumIdCategoria: categoriaSelecionada,
+			dataInicio: inputDataInicio,
+			dataFim: dataTermino,
+			horaInicio: inputHoraInicio,
+			horaFim: inputHoraFim,
+			capa: inputCapa,
+			cep: inputCep,
+			logradouro: inputLogradouro,
+			bairro: inputBairro,
+			cidade: inputCidade,
+			estado: inputEstado,
+			tblTipoEventoIdTipoEvento: tipoEventoSelecionado,
+		})
+		.then((response) => {
+			console.log(response);
+		});
+
+	// try {
+	//     const resposta = await fetch('http://localhost:4000/evento/cadastrarEventoEndereco/1/', {
+	//         method: 'POST',
+	//         headers: {
+	//             Accept: 'application/json',
+	//             'Content-Type': 'application/json'
+	//         },
+	//         body: JSON.stringify(evento)
+	//     })
+
+	// } catch (erro) {
+	//     console.error(erro)
+	// }
 }
 
 async function pegarCategoria() {
-    try {
-        const response = await fetch('http://localhost:4000/categoria/listarCategorias')
+	try {
+		const response = await fetch("http://localhost:4000/categoria/listarCategorias");
 
-        const data = await response.json()
+		const data = await response.json();
 
-        mostrarCategoria(data)
-
-    } catch (error) {
-
-        console.error(error)
-
-    }
+		mostrarCategoria(data);
+	} catch (error) {
+		console.error(error);
+	}
 }
 
-pegarCategoria()
+pegarCategoria();
 
 function mostrarCategoria(categorias) {
+	let output = "";
 
-    let output = ''
+	for (let categoria of categorias) {
+		output += `<option value="${categoria.idCategoria}">${categoria.nomeCategoria}</option>`;
+	}
 
-    for (let categoria of categorias) {
-        output += `<option value="${categoria.idCategoria}">${categoria.nomeCategoria}</option>`
-    }
-
-    document.querySelector('#categoryOption').innerHTML = output
-
+	document.querySelector("#categoryOption").innerHTML = output;
 }
 
+async function pegarTipoEvento() {
+	try {
+		const response = await fetch("http://localhost:4000/tipoEvento/listarTipoEvento");
 
-async function pegarTipoEvento(){
-    try {
-        const response = await fetch('http://localhost:4000/tipoEvento/listarTipoEvento')
+		const data = await response.json();
 
-        const data = await response.json()
-
-        mostrarTipoEvento(data)
-
-    } catch (error) {
-
-        console.error(error)
-
-    }
+		mostrarTipoEvento(data);
+	} catch (error) {
+		console.error(error);
+	}
 }
 
-pegarTipoEvento()
+pegarTipoEvento();
 
 function mostrarTipoEvento(tiposEvento) {
+	let output = "";
 
-    let output = ''
+	for (let tipoEvento of tiposEvento) {
+		output += `<option value="${tipoEvento.idTipoEvento}">${tipoEvento.tipo}</option>`;
+	}
 
-    for (let tipoEvento of tiposEvento) {
-        output += `<option value="${tipoEvento.idTipoEvento}">${tipoEvento.tipo}</option>`
-    }
-
-    document.querySelector('#tipoEventoOption').innerHTML = output
-
+	document.querySelector("#tipoEventoOption").innerHTML = output;
 }
