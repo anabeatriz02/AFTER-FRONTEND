35751abe9909a4859b56fb30923fe414315e799e
Fernanda Ferreira <79976892+fernandaferreiras@users.noreply.github.com>
2022-05-03 11:19:16 -0300

    ˜.˜

--- javascript/create-event/criarEvento.js
+++ javascript/create-event/criarEvento.js
@@ -3,7 +3,9 @@ const btn = document.querySelector("#salvar");
 btn.addEventListener("click", () => {
 	const evento = getDadosEvento();
 
-	enviarEventoParaAPI(evento);
+	// enviarEventoParaAPI(evento);
+
+	
 });
 
 function getDadosEvento() {
@@ -33,30 +35,54 @@ function getDadosEvento() {
 
 	const inputCapa = document.querySelector("#input-photo-file");
 
+
 	if (inputDataFim.value == null || inputDataFim.value == "") {
 		dataTermino = inputDataInicio.value;
 	} else {
-		dataTermino = inputDataFim.value;
+		dataTermino = çcçç.value;
 	}
 
-	const evento = {
-		titulo: inputTitulo.value,
-		descricao: inputDescricao.value,
-		tblCategoriumIdCategoria: categoriaSelecionada,
-		dataInicio: inputDataInicio.value,
-		dataFim: dataTermino,
-		horaInicio: inputHoraInicio.value,
-		horaFim: inputHoraFim.value,
-		capa: inputCapa.value,
-		cep: inputCep.value,
-		logradouro: inputLogradouro.value,
-		bairro: inputBairro.value,
-		cidade: inputCidade.value,
-		estado: inputEstado.value,
-		tblTipoEventoIdTipoEvento: tipoEventoSelecionado,
-	};
+	const formData = new FormData();
 
-	return evento;
+	formData.append("titulo", inputTitulo.value);
+	formData.append("descricao", inputDescricao.value);
+	formData.append("tblCategoriumIdCategoria", categoriaSelecionada);
+	formData.append("dataInicio", inputDataInicio.value);
+	formData.append("dataFim", dataTermino.value);
+	formData.append("horaInicio", inputHoraInicio.value);
+	formData.append("horaFim", inputHoraFim.value);
+	formData.append("capa", inputCapa.files[0]);
+	formData.append("cep", inputCep.value);
+	formData.append("logradouro", inputLogradouro.value);
+	formData.append("bairro", inputBairro.value);
+	formData.append("cidade", inputCidade.value);
+	formData.append("estado", inputEstado.value);
+	formData.append("tblTipoEventoIdTipoEvento", tipoEventoSelecionado);
+
+	axios.post("http://localhost:4000/evento/cadastrarEventoEndereco/1/", formData, {
+		headers: {
+			"Content-Type": "multipart/form-data",
+		},
+	});
+
+	// const evento = {
+	// 	titulo: inputTitulo.value,
+	// 	descricao: inputDescricao.value,
+	// 	tblCategoriumIdCategoria: categoriaSelecionada,
+	// 	dataInicio: inputDataInicio.value,
+	// 	dataFim: dataTermino,
+	// 	horaInicio: inputHoraInicio.value,
+	// 	horaFim: inputHoraFim.value,
+	// 	capa: inputCapa.value,
+	// 	cep: inputCep.value,
+	// 	logradouro: inputLogradouro.value,
+	// 	bairro: inputBairro.value,
+	// 	cidade: inputCidade.value,
+	// 	estado: inputEstado.value,
+	// 	tblTipoEventoIdTipoEvento: tipoEventoSelecionado,
+	// };
+
+	// return evento;
 }
 
 // btn.addEventListener("click", () => {
@@ -73,42 +99,10 @@ function getDadosEvento() {
 // 		});
 // });
 
-async function enviarEventoParaAPI(evento) {
-	axios
-		.post("https://reqres.in/api/login", {
-			titulo: inputTitulo,
-			descricao: inputDescricao,
-			tblCategoriumIdCategoria: categoriaSelecionada,
-			dataInicio: inputDataInicio,
-			dataFim: dataTermino,
-			horaInicio: inputHoraInicio,
-			horaFim: inputHoraFim,
-			capa: inputCapa,
-			cep: inputCep,
-			logradouro: inputLogradouro,
-			bairro: inputBairro,
-			cidade: inputCidade,
-			estado: inputEstado,
-			tblTipoEventoIdTipoEvento: tipoEventoSelecionado,
-		})
-		.then((response) => {
-			console.log(response);
-		});
+// async function enviarEventoParaAPI(evento) {
 
-	// try {
-	//     const resposta = await fetch('http://localhost:4000/evento/cadastrarEventoEndereco/1/', {
-	//         method: 'POST',
-	//         headers: {
-	//             Accept: 'application/json',
-	//             'Content-Type': 'application/json'
-	//         },
-	//         body: JSON.stringify(evento)
-	//     })
-
-	// } catch (erro) {
-	//     console.error(erro)
-	// }
-}
+	
+// }
 
 async function pegarCategoria() {
 	try {
