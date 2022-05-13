531dd172c122381bca315b9a8b9b956fe323451d
Fernanda Ferreira <79976892+fernandaferreiras@users.noreply.github.com>
2022-05-13 11:17:51 -0300

    .

--- javascript/create-event/modal.js
+++ javascript/create-event/modal.js
@@ -1,11 +1,11 @@
 function openModal() {
-	let modal = document.querySelector(".modal-form");
+	let modal = document.querySelector(".modal-form-window ");
 
 	modal.style.display = "block";
 }
 
 function closeModel() {
-	let modal = document.querySelector(".modal-form");
+	let modal = document.querySelector(".modal-form-window ");
 
 	modal.style.display = "none";
 }
