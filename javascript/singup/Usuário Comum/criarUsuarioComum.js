de3900edae04b8e6132fae21d61a3c888541ea0a
FelipeMel0 <felipedemfernandes@gmail.com>
2022-05-13 14:40:15 -0300

    Validando formulário de cadastro de usuário comum

--- javascript/singup/Usuário Comum/criarUsuarioComum.js
+++ javascript/singup/Usuário Comum/criarUsuarioComum.js
@@ -1,9 +1,8 @@
 "use-strict"
 
 function realizarCadastroUsuarioComum() {
-    const perfil = getDadosPerfil()
 
-    console.log(perfil)
+    const perfil = getDadosPerfil()
 
 }
 
@@ -25,16 +24,148 @@ function getDadosPerfil() {
 
     const inputBiografia = document.querySelector('#biografia')
 
-    if (inputSenhaConfirmar.value !== inputSenha.value) {
-        alert("A senha digitada está incorreta")
+    const isRequired = value => value === '' ? false : true;
 
-        const btnSalvar = document.querySelector('#salvarUsuario')
+    const checkNickname = () => {
+        let valid = false
 
-        btnSalvar.setAttribute('href', "./singup.html")
+        const nickname = inputNickname.value.trim()
 
-        return false
+        if (!isRequired(nickname)) {
+            alert("Você não passou o nickname")
+        } else {
+            valid = true
+        }
 
-    } else {
+        return valid
+    }
+
+    const checkEmail = () => {
+        let valid = false
+
+        const email = inputEmail.value.trim()
+
+        if (!isRequired(email)) {
+            alert("Você não passou o email")
+        } else {
+            valid = true
+        }
+
+        return valid
+    }
+
+    const checkNomeCompleto = () => {
+        let valid = false
+
+        const nomeCompleto = inputNomeCompleto.value.trim()
+
+        if (!isRequired(nomeCompleto)) {
+            alert("Você não passou o nome")
+        } else {
+            valid = true
+        }
+
+        return valid
+    }
+
+    const checkDataNasc = () => {
+        let valid = false
+
+        const dataNasc = inputDataNasc.value.trim()
+
+        if (!isRequired(dataNasc)) {
+            alert("Você não passou a data de nascimento")
+        } else {
+            valid = true
+        }
+
+        return valid
+    }
+
+    const checkCep = () => {
+        let valid = false
+
+        const cep = inputCep.value.trim()
+
+        if (!isRequired(cep)) {
+            alert("Você não passou o cep")
+        } else {
+            valid = true
+        }
+
+        return valid
+    }
+
+    const checkCidade = () => {
+        let valid = false
+
+        const cidade = inputCidade.value.trim()
+
+        if (!isRequired(cidade)) {
+            alert("Você não passou a cidade")
+        } else {
+            valid = true
+        }
+
+        return valid
+    }
+
+    const checkEstado = () => {
+        let valid = false
+
+        const estado = inputEstado.value.trim()
+
+        if (!isRequired(estado)) {
+            alert("Você não passou o estado")
+        } else {
+            valid = true
+        }
+
+        return valid
+    }
+
+    const checkSenha = () => {
+        let valid = false
+
+        const senha = inputSenha.value.trim()
+        const senhaConfirmar = inputSenhaConfirmar.value.trim()
+
+        if (senha != senhaConfirmar) {
+            alert("A senha está incorreta")
+        } else if(!isRequired(senha) && !isRequired(senhaConfirmar)){
+            alert("Você não passou a senha")
+        } else {
+            valid = true
+        }
+
+        return valid
+    }
+
+
+    //Validação dos campos
+
+    let isNicknameValid = checkNickname(),
+        isNomeValid = checkNomeCompleto(),
+        isEmailValid = checkEmail(),
+        isSenhaValid = checkSenha(),
+        isDataNascValid = checkDataNasc(),
+        isCepValid = checkCep(),
+        isCidadeValid = checkCidade(),
+        isEstadoValid = checkEstado()
+
+    let isFormValid =
+        isNicknameValid &&
+        isNomeValid &&
+        isEmailValid &&
+        isEstadoValid &&
+        isCidadeValid &&
+        isSenhaValid &&
+        isCepValid &&
+        isDataNascValid
+
+    //Quando o formulário for válido, fazer o post desse evento
+    if (isFormValid) {
+        alert("Conta cadastrada com sucesso!")
 
         const formData = new FormData()
 
@@ -55,15 +186,6 @@ function getDadosPerfil() {
                 'Content-Type': 'multipart/form-data'
             }
         })
-
-        // axios(config)
-        //     .then(function (response) {
-        //         console.log(JSON.stringify(response.data));
-        //     })
-        //     .catch(function (error) {
-        //         console.log(error);
-        //     });
-
     }
 
 }
