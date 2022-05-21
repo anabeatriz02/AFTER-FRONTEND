5ed7d4c9f4a5e14743c0f6a441c2146ef37f8730
FelipeMel0 <felipedemfernandes@gmail.com>
2022-05-21 12:29:49 -0300

    Realizando teste de login

---
+++ javascript/signin/logar.js
@@ -0,0 +1,48 @@
+const form = document.querySelector("#formLogin")
+
+const inputEmail = document.querySelector("#inputEmail")
+const inputSenha = document.querySelector("#inputSenha")
+
+const btnEntrar = document.querySelector("#btnEntrar")
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    var data = JSON.stringify({
+        "email": inputEmail.value,
+        "senha": inputSenha.value
+    });
+
+    var config = {
+        method: 'post',
+        url: 'http://localhost:4000/perfil/login',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    }
+
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+
+            localStorage.setItem("token", "Bearer " + response.data.token)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+
+})
+
+
+
+const usuarioAutenticado = () => {
+    axios.get("http://localhost:4000/perfil/estaLogado", {
+        headers: {
+            "Authorization": localStorage.getItem("token")
+        }
+    }).then((response) => {
+        console.log(response)
+    })
+}
