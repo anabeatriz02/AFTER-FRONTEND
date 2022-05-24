e649fc94345b82cd2914c3838fc582d431380877
FelipeMel0 <felipedemfernandes@gmail.com>
2022-05-24 10:17:59 -0300

    Alterando listagem de imagens em carrossel

--- javascript/event-description/carousel.js
+++ javascript/event-description/carousel.js
@@ -1,18 +1,21 @@
-$(".owl-slider").owlCarousel({
-	loop: true,
-	nav: false,
-	autoplay: true,
-	autoplayTimeout: 3000,
-	dots: true,
-	responsive: {
-		0: {
-			items: 1,
+function mostrarCarrossel() {
+	$(".owl-slider").owlCarousel({
+		loop: true,
+		nav: false,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		dots: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 1,
+			},
+			1000: {
+				items: 1,
+			},
 		},
-		600: {
-			items: 1,
-		},
-		1000: {
-			items: 1,
-		},
-	},
-});
+	});
+	
+}
--- javascript/event-description/mostrarImagens.js
+++ javascript/event-description/mostrarImagens.js
@@ -1,7 +1,7 @@
 async function getContent() {
     try {
 
-        const response = await fetch('http://localhost:4000/imagensEvento/listarImagensIdEvento/28')
+        const response = await fetch('http://localhost:4000/imagensEvento/listarImagensIdEvento/79')
 
         console.log(response)
 
@@ -22,13 +22,24 @@ async function getContent() {
 getContent()
 
 function mostrarImagens(imagens){
-    let output = "";
+
+	const containerImagens = document.querySelector("#imagens");
+
+    let output = [];
 
 	for (let imagem of imagens) {
-		output += `<img src="http://localhost:4000/${imagem.imagem}" />`;
+        const div = document.createElement('div');
+        div.className = 'item-photo';
+
+        const img = document.createElement('img');
+        img.src = `http://localhost:4000/${imagem.imagem}`;
+
+        div.appendChild(img)
+
+        containerImagens.appendChild(div)
+
+		// output += `<div class="item-photo" id="imgTeste4"><img src="http://localhost:4000/${imagem.imagem}" /></div>`;
 	}
 
-    // return output
-
-	// document.querySelector("#imagens").innerHTML = output
+    mostrarCarrossel();
 }
